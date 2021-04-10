775ed203a7f194d3a7e5cb41706de75f2049c485
thiagomedina <thiagomedina001@gmail.com>
2021-04-10 16:54:27 -0300

    styled

--- src/components/Barrier/index.js
+++ src/components/Barrier/index.js
@@ -1,12 +1,22 @@
 import { component } from 'haunted'
 import { html } from 'lit-html'
-import {Container} from './styles/Barrier'
+import styled from 'masquerades'
+
+
+const Container = styled.h1`
+background-color: red;
+
+`;
+
+customElements.define('styled-container', Container, {
+    extends: 'h1',
+});
+
 
 
 const Barrier = () => html`
-    <Container>
-    <h1 class="title">TEste</h1>
-    </Container>
+   
+    <h1 is="styled-container">TEste</h1>
     `
 
 customElements.define('wc-title', component(Barrier))
